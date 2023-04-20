0fc5803a297f1aff3f0927c84b3ae7c37445dad2
shahid <shahdbest@gmail.com>
2023-04-20 10:55:45 +0530

    question

---
+++ src/main/java/com/stackroute/exercises/FindDay.java
@@ -0,0 +1,10 @@
+package com.stackroute.exercises;
+
+
+public class FindDay {
+
+    //write logic to find day of the date and return result
+    public String findDay(int month, int day, int year) {
+        return null;
+    }
+}
